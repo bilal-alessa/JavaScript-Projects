7af2bf621f96da72415894626011d4eb83c133b1
Bilal Al-Essa <bilal.al-essa@outlook.com>
2020-09-15 23:09:47 -0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
-var X = "Hello, World!", Y = " the temperature might be ";
-var Y = Y.fontcolor("red");
-window.alert(X);
-document.write(X + Y);
-document.write(35 + 60)
+var X = "Hello, World!", Y = " the temperature might be "; // Assigning 2 variables.
+var Y = Y.fontcolor("red"); // Assigning one variable to have a font color change.
+window.alert(X);    // Window alert to open as the page is interpreted.
+document.write(X + Y); // Printed on screen the variables concatenated amongst each other.
+document.write(35 + 60) // Both variables added together from an expression.
