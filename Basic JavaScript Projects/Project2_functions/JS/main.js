eda29792250d6f315b772be4237df2fec886d22e
Bilal Al-Essa <bilal.al-essa@outlook.com>
2020-09-16 11:58:53 -0700

    Upload Project2_Function
    Uploading assignment files.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function function20() {
+    var sent1 = "Hello, "; //Sets the first variable up.
+    sent1 += "World!";  //Sets the 2nd variable up and the operator(+) concatinates the variables.
+    document.getElementById("paragraph1").innerHTML = sent1; // The method gets the id and sets the html element by ID.
+}
