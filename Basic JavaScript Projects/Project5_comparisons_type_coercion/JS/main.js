99e6eac9552dc0f7d93d1736c769ae160ea5d732
Bilal Al-Essa <bilal.al-essa@outlook.com>
2020-09-17 12:42:26 -0700

    Project5 Coercion
    Uploading Project 5 Type Coercion.

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,77 @@
+function PrintType() {
+    x = "Word";
+    document.getElementById("para1").innerHTML = document.write(typeof x);
+}
+
+function NotaNumber() {
+    x = 0 / 0;
+    document.getElementById("Notanumber").innerHTML = x;
+    
+}
+
+function NotaNumber() {
+    x = 5+2;
+    document.getElementById("Notanumber").innerHTML = x;
+    document.getElementById("realnumber").innerHTML = isNaN(x);
+}
+
+function FakeNumber() {
+    x = 0 / 0;
+    document.getElementById("fakenumber").innerHTML = isNaN(x);
+}
+
+function infinite() {
+    document.getElementById("infinite number").innerHTML = 2e310;
+}
+
+function booleanaction() {
+    
+    document.getElementById("boolean").innerHTML = (50 > 30 );
+}
+
+console.log(80 < 50);
+
+function coercion() {
+    document.getElementById("coercion").innerHTML = ("100" + 20);
+}
+
+function dataandvalue() {
+    var x = 20;
+    var y = 20;
+    document.getElementById("dataandvalue").innerHTML = (x === y);
+}
+
+function dataandvalue2() {
+    var x = "HELLO";
+    var y = 10;
+    document.getElementById("dataandvalue2").innerHTML = (x === y);
+}
+
+
+
+function dataandvalue3() {
+    var x = "10";
+    var y = 10;
+    document.getElementById("dataandvalue3").innerHTML = (x === y);
+}
+
+function dataandvalue4() {
+    var x = 5;
+    var y = 10;
+    document.getElementById("dataandvalue4").innerHTML = (x === y);
+}
+
+function andsign() {
+    var x = 5;
+    var y = 6;
+    document.getElementById("andsign").innerHTML = (y + x == 11);
+}
+
+function orsign() {
+
+    document.getElementById("orsign").innerHTML = (5 > 13 || 8 > 13);
+}
+
+function negative() {
+    document.getElementById("negative").innerHTML = !( 5 > 3);
+}
